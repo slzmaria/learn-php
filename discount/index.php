b93bf67985a8cd98760924b7643b56b0574879f2
Slizovskaya <maria.slizovskaya@gmail.com>
2020-04-20 10:44:17 +0300

    Order info with discount, check doesn't work

--- discount/index.php
+++ discount/index.php
@@ -1,22 +1,55 @@
 <html>
-<head></head>
-<body>
-    <h1>
-     Bill:  
-    <?php
-       
+ <head>
+ </head>
+ <body>
+   <?php
+        $product = $_GET['product'];
         $qua = $_GET['quantity'];
         $price = $_GET['price'];
         $dis = $_GET['discount'];
-        $bill = $qua * $price;
+        $sum = $qua * $price;
 
-      
         if ($dis !='') {
-           $bill = ($qua * $price) - (($qua * $price) * ($dis/100));
+           $sum = $sum - $sum * ($dis/100);
        }
-        echo $bill;
-    ?>
-   
- </h1>
-</body>
+    //    if (!is_numeric ($qua) || !is_numeric ($price));
+    //   {
+    //   echo 'Oops..Something went wrong';
+    //   exit;
+    //   }
+    //   if (($qua <= '0') || ($price <= '0'));
+    //   {
+    //   echo 'Oops..Something went wrong';
+    //   exit;
+    //   }
+  
+   ?>
+    
+  <table border="1">
+   <caption>Order information</caption>
+   <tr>
+       <th>Product name</th>
+       <th>Quantity</th>
+       <th>Price</th>
+       <th>Discount</th>
+       <th>Summary</th>
+   </tr>
+   <tr>
+    <td><?php echo $product ?> </td>
+    <td><?php echo $qua ?></td>
+    <td><?php echo $price ?></td>
+    <td><?php  
+         if ($dis == '') {
+          echo 'No';
+        } else {
+       echo $dis, '%';
+        } ?></td>
+    <td><?php 
+         if ($dis == '') {
+         echo $sum;
+        } else {
+       echo $sum;
+        } ?></td>   
+   </tr>
+ </body>
 </html>
